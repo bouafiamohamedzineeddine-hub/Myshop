7fb8d48c63bdf7e2c9fc38cefd31cdb31d6c2768
abderhmane092 <abderhmane092@gmail.com>
2026-04-17 18:56:36 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template,redirect,url_for
+from flask import Flask ,render_template,redirect,url_for,session,redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, login_required, LoginManager,logout_user,current_user
 from flask_wtf import FlaskForm
